30d7ac57d2ed52ec9018066f5d621eae9ffbb2c0
abduelrahman <a.emam@sbs-sa.net>
2024-02-29 16:14:23 +0200

    Upgrade.

--- leon-utilities/src/main/java/am/leon/utilities/android/extentions/MultipartBodyExt.kt
+++ leon-utilities/src/main/java/am/leon/utilities/android/extentions/MultipartBodyExt.kt
@@ -1,64 +1,77 @@
-//package am.leon.utilities.extentions
+package am.leon.utilities.android.extentions
 //
+//import android.webkit.MimeTypeMap
 //import okhttp3.MediaType.Companion.toMediaType
 //import okhttp3.MultipartBody
 //import okhttp3.RequestBody
 //import okhttp3.RequestBody.Companion.asRequestBody
-//import okhttp3.RequestBody.Companion.toRequestBody
 //import java.io.File
 //
 //enum class FileMediaType(val mediaType: okhttp3.MediaType) {
 //    FILE("multipart/form-data".toMediaType()),
 //    IMAGE("image/*".toMediaType()),
-//    TEXT("text/plain".toMediaType())
+//    TEXT("text/plain".toMediaType());
+//
+//    companion object {
+//        fun findIsImageOrFile(file: File): FileMediaType {
+//            val extension = file.path.substringAfterLast('.', "")
+//            val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+//
+//            // Regular expression to match image MIME types
+//            val imageRegex = Regex("^image/.*$")
+//
+//            // Check if the MIME type starts with "image/" or if the extension is commonly used for images
+//            return if (mimeType != null && mimeType.matches(imageRegex))
+//                IMAGE
+//            else
+//                FILE
+//        }
+//    }
 //}
 //
-//fun List<File>.getFileMediaListAsPart(
-//    fieldKey: String, fileMediaType: FileMediaType = FileMediaType.IMAGE
-//): ArrayList<MultipartBody.Part> {
+//fun List<File>.getFileMediaListAsPart(fieldKey: String): ArrayList<MultipartBody.Part> {
 //    val parts: ArrayList<MultipartBody.Part> = ArrayList()
 //    forEachIndexed { index, file ->
-//        parts.add(
-//            get(index).getFileMediaAsPart(fieldKey.plus("[".plus(file).plus("]")), fileMediaType)
-//        )
+//        parts.add(get(index).getFileMediaAsPart(fieldKey.plus("[".plus(file).plus("]"))))
 //    }
 //    return parts
 //}
 //
-//fun File.getFileMediaAsPart(fieldKey: String, fileMediaType: FileMediaType): MultipartBody.Part {
-//    val body: RequestBody = asRequestBody(fileMediaType.mediaType)
+//fun File.getFileMediaAsPart(fieldKey: String): MultipartBody.Part {
+//    val body: RequestBody = asRequestBody(FileMediaType.findIsImageOrFile(this).mediaType)
 //    return MultipartBody.Part.createFormData(fieldKey, name, body)
 //}
 //
 //fun String.toRequestBody(): RequestBody = toRequestBody(FileMediaType.TEXT.mediaType)
+
+
+//private fun getParamsAsPart(key: String, value: String): MultipartBody.Part {
+//    return MultipartBody.Part.createFormData(key, value)
+//}
 //
-////private fun getParamsAsPart(key: String, value: String): MultipartBody.Part {
-////    return MultipartBody.Part.createFormData(key, value)
-////}
-////
-////protected fun getParamsAsPart(
-////    type: String, value: String, status: Boolean
-////): MultipartBody.Part? {
-////    return if (!status) MultipartBody.Part.createFormData(type, value) else null
-////}
+//protected fun getParamsAsPart(
+//    type: String, value: String, status: Boolean
+//): MultipartBody.Part? {
+//    return if (!status) MultipartBody.Part.createFormData(type, value) else null
+//}
+
+//fun getStringListAsPart(
+//    key: String, list: List<String>
+//): ArrayList<MultipartBody.Part> {
+//    val parts: ArrayList<MultipartBody.Part> = ArrayList()
 //
-////fun getStringListAsPart(
-////    key: String, list: List<String>
-////): ArrayList<MultipartBody.Part> {
-////    val parts: ArrayList<MultipartBody.Part> = ArrayList()
-////
-////    for (item in list.indices) {
-////        parts.add(getParamsAsPart(key.plus("[".plus(item).plus("]")), list[item]))
-////    }
-////    return parts
-////}
-////
-////
-////fun String.getComponentListAsPart(list: List<Tag>): ArrayList<MultipartBody.Part> {
-////    val parts: ArrayList<MultipartBody.Part> = ArrayList()
-////
-////    for (item in list.indices) {
-////        parts.add(getParamsAsPart(this.plus("[".plus(item).plus("]")), list[item].name))
-////    }
-////    return parts
-////}
+//    for (item in list.indices) {
+//        parts.add(getParamsAsPart(key.plus("[".plus(item).plus("]")), list[item]))
+//    }
+//    return parts
+//}
+//
+//
+//fun String.getComponentListAsPart(list: List<Tag>): ArrayList<MultipartBody.Part> {
+//    val parts: ArrayList<MultipartBody.Part> = ArrayList()
+//
+//    for (item in list.indices) {
+//        parts.add(getParamsAsPart(this.plus("[".plus(item).plus("]")), list[item].name))
+//    }
+//    return parts
+//}
